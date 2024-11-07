8b24d393bb126e863aa56796a1868d2a762cb30c
RDMillen <rossmillen@protonmail.com>
2024-11-07 14:40:59 +0000

    Added light sensor, display settings. Expanded README.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # EnviroPy
 This is the base code for using a Raspberry Pi with Enviro Plus hat for monitoring and logging gas level data. 
+
+### Raspberry Pi Setup
+Ensure your Raspberry Pi is running Pi OS Bookworm (Debian 12.0) or later. Ensure your OS is up to date by running the following commands:
+```bash
+    sudo apt update
+    sudo apt upgrade
+```
+
+Next, open a new terminal and then type the following:
+```bash
+    git clone https://github.com/pimoroni/enviroplus-python
+    cd enviroplus-python
+    ./install.sh
+```
+
+Once that is done, reboot your Raspberry Pi.
+```bash
+    sudo systemctl reboot
+```
+
+The EnviroPlus-Python repository contains a selection of example pieces of code to demonstrate the function of each sensor on the EnviroPlus sensor board. 
--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 import time
 from smbus2 import SMBus
 from bme280 import BME280
+from ltr559 import LTR559
+from enviroplus import gas
+import st7735
+from PIL import Image, ImageDraw, ImageFont
+
+# Define sensors
 
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
+ltr = LTR559()
 
-# Test Temperature, Pressure, and Humidity
+# Define Screen
+
+disp = st7735.ST7735(
+    port=0,
+    cs=1,
+    dc="GPIO9",
+    backlight="GPIO12",
+    rotation=270,
+    spi_speed_hz=10000000
+)
+
+
+# Read gas levels from MICS6814 Module
+def gasRead():
+    while True:
+        readings = gas.read_all()
+        print(readings)
+        time.sleep(1.0)
+
+# Test Temperature, Pressure, Humidity, Gas Levels
 print(bme280.get_temperature())
 print(bme280.get_pressure())
 print(bme280.get_humidity())
+print(gasRead())
